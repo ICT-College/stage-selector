eac4f6847739ade028617dacbf9ff9748a19daf4
Marlin Cremers <m.cremers@cvo-technologies.com>
2015-09-22 12:51:07 +0200

    Add comments to shard filter

--- src/Routing/Filter/ShardFilter.php
+++ src/Routing/Filter/ShardFilter.php
@@ -2,17 +2,24 @@
 
 namespace App\Routing\Filter;
 
-use Cake\Event\Event;
 use Cake\Datasource\ConnectionManager;
+use Cake\Event\Event;
 use Cake\Network\Request;
 use Cake\Routing\DispatcherFilter;
 
 class ShardFilter extends DispatcherFilter
 {
 
+    /**
+     * Sets up database connection based on subdomain
+     *
+     * @param Event $event event containing request data
+     *
+     * @return void
+     */
     public function beforeDispatch(Event $event)
     {
-        /** @var Request $request */
+        /* @var Request $request */
         $request = $event->data['request'];
 
         ConnectionManager::alias('shard_' . $request->subdomains()[0], 'default');
