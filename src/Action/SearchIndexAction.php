ddc9f9fe1a32ea23e81289ac3c30ea167bdae65d
Marlin Cremers <m.cremers@cvo-technologies.com>
2015-09-23 13:54:16 +0200

    Add search parameters to API response

--- src/Action/SearchIndexAction.php
+++ src/Action/SearchIndexAction.php
@@ -6,19 +6,26 @@ use Crud\Action\IndexAction;
 
 class SearchIndexAction extends IndexAction
 {
+
     protected function _handle()
     {
         $this->_controller()->Prg->commonProcess();
 
-        $query = $this->_table()->find('searchable', $this->_controller()->Prg->parsedParams());
+        $searchParameters = $this->_controller()->Prg->parsedParams();
+
+        $query = $this->_table()->find('searchable', $searchParameters);
         $subject = $this->_subject(['success' => true, 'query' => $query]);
 
         $this->_trigger('beforePaginate', $subject);
         $items = $this->_controller()->paginate($subject->query);
         $subject->set(['entities' => $items]);
 
+        $this->_controller()->set(['search' => [
+            'parameters' => $searchParameters
+        ]]);
+        $this->serialize(['search' => 'search']);
+
         $this->_trigger('afterPaginate', $subject);
         $this->_trigger('beforeRender', $subject);
-
     }
 }
