6fb6cae8a745be782164c12fa5450dcbae809707
Marlin Cremers <m.cremers@cvo-technologies.com>
2015-10-06 09:35:06 +0200

    Remove drop statements from database schema

--- config/schema/database.sql
+++ config/schema/database.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Oct 06, 2015 at 09:32 AM
+-- Generation Time: Oct 06, 2015 at 09:34 AM
 -- Server version: 5.6.25-0ubuntu0.15.04.1
 -- PHP Version: 5.6.4-4ubuntu6.3
 
@@ -19,7 +19,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `school_stage_selector_ictcollege`
 --
-DROP DATABASE `school_stage_selector_ictcollege`;
 
 -- --------------------------------------------------------
 
@@ -28,16 +27,16 @@ DROP DATABASE `school_stage_selector_ictcollege`;
 --
 
 CREATE TABLE IF NOT EXISTS `companies` (
-`id` int(11) NOT NULL,
-  `stagemarkt_id` varchar(16) COLLATE utf8_bin DEFAULT NULL,
-  `name` varchar(64) CHARACTER SET utf8 NOT NULL,
-  `address` varchar(64) CHARACTER SET utf8 NOT NULL,
-  `postcode` char(6) COLLATE utf8_bin NOT NULL,
-  `city` varchar(64) CHARACTER SET utf8 NOT NULL,
-  `country` char(2) COLLATE utf8_bin NOT NULL,
-  `coordinates` point DEFAULT NULL,
-  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    `id` int(11) NOT NULL,
+    `stagemarkt_id` varchar(16) COLLATE utf8_bin DEFAULT NULL,
+    `name` varchar(64) CHARACTER SET utf8 NOT NULL,
+    `address` varchar(64) CHARACTER SET utf8 NOT NULL,
+    `postcode` char(6) COLLATE utf8_bin NOT NULL,
+    `city` varchar(64) CHARACTER SET utf8 NOT NULL,
+    `country` char(2) COLLATE utf8_bin NOT NULL,
+    `coordinates` point DEFAULT NULL,
+    `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB AUTO_INCREMENT=16809 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
@@ -47,9 +46,9 @@ CREATE TABLE IF NOT EXISTS `companies` (
 --
 
 CREATE TABLE IF NOT EXISTS `placement_students` (
-`id` int(11) NOT NULL,
-  `position_id` int(11) NOT NULL,
-  `student_id` int(11) NOT NULL
+    `id` int(11) NOT NULL,
+    `position_id` int(11) NOT NULL,
+    `student_id` int(11) NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
@@ -59,17 +58,17 @@ CREATE TABLE IF NOT EXISTS `placement_students` (
 --
 
 CREATE TABLE IF NOT EXISTS `positions` (
-`id` int(32) NOT NULL,
-  `stagemarkt_id` int(11) DEFAULT NULL,
-  `learning_pathway` varchar(8) COLLATE utf8_bin NOT NULL,
-  `description` text CHARACTER SET utf8,
-  `start` date DEFAULT NULL,
-  `end` date DEFAULT NULL,
-  `amount` int(3) NOT NULL,
-  `company_id` int(11) NOT NULL,
-  `study_program_id` int(8) NOT NULL,
-  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    `id` int(32) NOT NULL,
+    `stagemarkt_id` int(11) DEFAULT NULL,
+    `learning_pathway` varchar(8) COLLATE utf8_bin NOT NULL,
+    `description` text CHARACTER SET utf8,
+    `start` date DEFAULT NULL,
+    `end` date DEFAULT NULL,
+    `amount` int(3) NOT NULL,
+    `company_id` int(11) NOT NULL,
+    `study_program_id` int(8) NOT NULL,
+    `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB AUTO_INCREMENT=33092 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
@@ -78,8 +77,8 @@ CREATE TABLE IF NOT EXISTS `positions` (
 -- Stand-in structure for view `study_programs`
 --
 CREATE TABLE IF NOT EXISTS `study_programs` (
-`id` int(11)
-,`description` text
+     `id` int(11)
+    ,`description` text
 );
 -- --------------------------------------------------------
 
@@ -98,19 +97,19 @@ CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW 
 -- Indexes for table `companies`
 --
 ALTER TABLE `companies`
- ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `stagemarkt_id` (`stagemarkt_id`);
+ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `stagemarkt_id` (`stagemarkt_id`);
 
 --
 -- Indexes for table `placement_students`
 --
 ALTER TABLE `placement_students`
- ADD PRIMARY KEY (`id`);
+ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `positions`
 --
 ALTER TABLE `positions`
- ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `stagemarkt_id` (`stagemarkt_id`);
+ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `stagemarkt_id` (`stagemarkt_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -133,40 +132,39 @@ ALTER TABLE `positions`
 MODIFY `id` int(32) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=33092;--
 -- Database: `school_stage_selector_main`
 --
-DROP DATABASE `school_stage_selector_main`;
 
 DELIMITER $$
 --
 -- Functions
 --
 CREATE DEFINER=`root`@`localhost` FUNCTION `DISTANCE`(`point1` POINT, `point2` POINT) RETURNS float
-    DETERMINISTIC
-BEGIN
-	RETURN HAVERSINE(point1, point2) * 111.045;
-END$$
+DETERMINISTIC
+    BEGIN
+        RETURN HAVERSINE(point1, point2) * 111.045;
+    END$$
 
 CREATE DEFINER=`root`@`localhost` FUNCTION `haversine`(`point1` POINT, `point2` POINT) RETURNS float
-    NO SQL
-    DETERMINISTIC
+NO SQL
+DETERMINISTIC
     COMMENT 'Returns the distance in degrees on the Earth             between two known points of latitude and longitude'
-BEGIN
-	DECLARE lat1 FLOAT;
-    DECLARE lat2 FLOAT;
-    DECLARE lon1 FLOAT;
-    DECLARE lon2 FLOAT;
+    BEGIN
+        DECLARE lat1 FLOAT;
+        DECLARE lat2 FLOAT;
+        DECLARE lon1 FLOAT;
+        DECLARE lon2 FLOAT;
 
-    SET lat1 = X(point1);
-    SET lat2 = X(point2);
-    SET lon1 = Y(point1);
-    SET lon2 = Y(point2);
+        SET lat1 = X(point1);
+        SET lat2 = X(point2);
+        SET lon1 = Y(point1);
+        SET lon2 = Y(point2);
 
-    RETURN DEGREES(ACOS(
-              COS(RADIANS(lat1)) *
-              COS(RADIANS(lat2)) *
-              COS(RADIANS(lon2) - RADIANS(lon1)) +
-              SIN(RADIANS(lat1)) * SIN(RADIANS(lat2))
-            ));
-END$$
+        RETURN DEGREES(ACOS(
+                           COS(RADIANS(lat1)) *
+                           COS(RADIANS(lat2)) *
+                           COS(RADIANS(lon2) - RADIANS(lon1)) +
+                           SIN(RADIANS(lat1)) * SIN(RADIANS(lat2))
+                       ));
+    END$$
 
 DELIMITER ;
 
@@ -177,8 +175,8 @@ DELIMITER ;
 --
 
 CREATE TABLE IF NOT EXISTS `study_programs` (
-`id` int(11) NOT NULL,
-  `description` text CHARACTER SET utf8 NOT NULL
+    `id` int(11) NOT NULL,
+    `description` text CHARACTER SET utf8 NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=25188 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
@@ -189,7 +187,7 @@ CREATE TABLE IF NOT EXISTS `study_programs` (
 -- Indexes for table `study_programs`
 --
 ALTER TABLE `study_programs`
- ADD PRIMARY KEY (`id`);
+ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
