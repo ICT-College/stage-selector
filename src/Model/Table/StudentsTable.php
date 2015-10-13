47918d1eaf75d11483348fe40fb37c54d69c1b3c
Marlin Cremers <m.cremers@cvo-technologies.com>
2015-10-13 14:10:55 +0200

    Use encryption in the students table

---
+++ src/Model/Table/StudentsTable.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Model\Table;
+
+use Cake\Database\Schema\Table as Schema;
+use Cake\ORM\Table;
+
+class StudentsTable extends Table
+{
+
+    /**
+     * @inheritDoc
+     */
+    protected function _initializeSchema(Schema $table)
+    {
+        $table->columnType('initials', 'encrypted');
+        $table->columnType('firstname', 'encrypted');
+        $table->columnType('insertion', 'encrypted');
+        $table->columnType('lastname', 'encrypted');
+        $table->columnType('email', 'encrypted');
+        $table->columnType('address', 'encrypted');
+        $table->columnType('postcode', 'encrypted');
+        $table->columnType('city', 'encrypted');
+        $table->columnType('telephone', 'encrypted');
+        $table->columnType('birthday', 'encrypted');
+        $table->columnType('birthplace', 'encrypted');
+
+        return $table;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public static function defaultConnectionName()
+    {
+        return 'secured';
+    }
+}
