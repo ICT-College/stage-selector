6903d7291dc09e73302c0ef4c06e4cd8095eca3b
Marlin Cremers <m.cremers@cvo-technologies.com>
2015-10-13 14:10:26 +0200

    Add a basic type implementation to do encryption

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -213,3 +213,4 @@ DispatcherFactory::add('ControllerFactory');
 Type::build('date')->useLocaleParser();
 Type::build('datetime')->useLocaleParser();
 Type::map('point', 'App\Database\Type\PointType');
+Type::map('encrypted', 'App\Database\Type\EncryptedType');
---
+++ src/Database/Type/EncryptedType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Database\Type;
+
+use Cake\Database\Driver;
+use Cake\Database\Type;
+use Cake\Utility\Security;
+
+class EncryptedType extends Type
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function toDatabase($value, Driver $driver)
+    {
+        if (is_null($value)) {
+            return null;
+        }
+
+        return Security::encrypt(serialize($value), md5('aaa'));
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function toPHP($value, Driver $driver)
+    {
+        if (is_null($value)) {
+            return null;
+        }
+
+        return unserialize(Security::decrypt($value, md5('aaa')));
+    }
+}
