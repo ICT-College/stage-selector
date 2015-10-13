070bbfae507519ac21a02ac62ce62040cc25f290
Wouter0100 <wouter@wouter0100.nl>
2015-10-13 15:30:28 +0200

    Added validation for the users table

--- src/Model/Table/UsersTable.php
+++ src/Model/Table/UsersTable.php
@@ -26,4 +26,41 @@ class UsersTable extends Table
     {
         return 'main';
     }
+
+    /**
+     * Validation for users table
+     *
+     * @param Validator $validator
+     * @return Validator
+     */
+    public function validationDefault(Validator $validator)
+    {
+        $validator->requirePresence('email')
+            ->add('email', [
+                'valid' => [
+                    'rule' => 'email',
+                    'message' => 'E-mail must be valid'
+                ],
+                'unique' => [
+                    'rule' => 'validateUnique',
+                    'provider' => 'table',
+                    'message' => 'E-mail must be unique'
+                ]
+            ])
+            ->requirePresence('firstname')
+            ->notEmpty('firstname', 'Firstname cannot be left blank')
+            ->requirePresence('lastname')
+            ->notEmpty('lastname', 'Lastname cannot be left blank')
+            ->requirePresence('student_number')
+            ->notEmpty('student_number', 'Student number cannot be left blank')
+            ->add('student_number', [
+                'unique' => [
+                    'rule' => 'validateUnique',
+                    'provider' => 'table',
+                    'message' => 'Student number must be unique'
+                ]
+            ]);
+
+        return $validator;
+    }
 }
