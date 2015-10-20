68b1379b3c8d8f2c3dddcdd68e301ea13fb9f27c
Wouter0100 <wouter@wouter0100.nl>
2015-10-20 16:11:20 +0200

    Added Positions controller

---
+++ src/Controller/Admin/PositionsController.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Controller\Admin;
+
+use Cake\Datasource\ConnectionManager;
+use CvoTechnologies\Gearman\JobAwareTrait;
+
+class PositionsController extends AppController
+{
+
+}
